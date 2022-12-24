16ed69cce9d3711e805b223bebc2e893573a0147
RounabhSahu <99762227+RounabhSahu@users.noreply.github.com>
2022-12-24 17:26:20 +0530

    keys r mixing up with function

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 
-// chanage keys value
-const keys = "CABDAAAAACDAADBABAABCAABDBABBCDABCCBCDAD";
+
+const ans = "CBBAADBADCCBABBCDCDABCBBDDAABADCCBCBACDABBCAABABBCBBCAAABC";
 const len = keys.length;
 
 let i = 0;
 
-const inter = setInterval(
+let inter = setInterval(
   () => {
-    document.querySelector(`#${keys[i]}_${i+1}`).click(); // select the correct option
-    document.querySelector("#main_div > div.tableWidthPercent > div.onlineTestLeftDiv > div.qnav > span.saveNextButton > a").click(); // move to next question
+    document.querySelector(`#${ans[i]}_${i+1}`).click(); 
+    document.querySelector(".button.savenext").click(); 
     i++;
     
-    if(i == len){ // stop the interval when all questions are answered
+    if(i == len){ 
       clearInterval(inter)
-      document.querySelector("#activator").click(); // submit the test
-      document.querySelector(`#box > div > div > div > div > input[type=radio]:nth-child(1)`).click(); // select the reason for submit
-      document.querySelector("#close_confirmed").click(); // submit the test
+      
     }
   }
-  // 2 seconds(2000 milliseconds) for each question
+  
 ,2000);
